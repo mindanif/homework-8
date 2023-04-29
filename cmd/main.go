125bae82fe89d0568e493b8f5a230fa8fc7f13df
mindanif <mindanif.728@gmail.com>
2023-04-29 20:02:43 +0300

    [GOJHW-8]: 1

--- cmd/main.go
+++ cmd/main.go
@@ -77,15 +77,12 @@ func main() {
 	}
 }
 func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
-	// Create the Jaeger exporter
 	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
 	if err != nil {
 		return nil, err
 	}
 	tp := tracesdk.NewTracerProvider(
-		// Always be sure to batch in production.
 		tracesdk.WithBatcher(exp),
-		// Record information about this application in a Resource.
 		tracesdk.WithResource(resource.NewWithAttributes(
 			semconv.SchemaURL,
 			semconv.ServiceName(service),
